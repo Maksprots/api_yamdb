76711969b33eb98ec9ba1d1a9893a92fa5696279
Anton Emelyanov <mail@technola.ru>
2023-01-23 15:04:26 +0500

    Добавлены endpoints и сериализаторы

---
+++ api_yamdb/api/v1/serializers.py
@@ -0,0 +1,127 @@
+import datetime as dt
+
+from rest_framework import serializers
+from rest_framework.relations import SlugRelatedField
+
+from reviews.models import Title, Category, Genre, User, Review, Comment
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        exclude = ('id',)
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        exclude = ('id',)
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    category = SlugRelatedField(
+        queryset=Category.objects.all(),
+        slug_field='slug',
+    )
+    genre = SlugRelatedField(
+        queryset=Genre.objects.all(),
+        slug_field='slug',
+        many=True
+    )
+    rating = serializers.IntegerField(read_only=True)
+
+    class Meta:
+        model = Title
+        fields = (
+            'id',
+            'name',
+            'year',
+            'rating',
+            'category',
+            'genre',
+            'description',
+        )
+
+    def validate_year(self, value):
+        year = dt.date.today().year
+        if not (value < year):
+            raise serializers.ValidationError('Проверьте год произведения!')
+        return value
+
+
+class TitleReadSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(read_only=True)
+    genre = GenreSerializer(read_only=True, many=True)
+    rating = serializers.IntegerField(read_only=True)
+
+    class Meta:
+        model = Title
+        fields = (
+            'id',
+            'name',
+            'year',
+            'rating',
+            'category',
+            'genre',
+            'description',
+        )
+
+
+class UsersSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        fields = (
+            'username',
+            'email',
+            'first_name',
+            'last_name',
+            'bio',
+            'role',
+        )
+        model = User
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        required=False,
+        read_only=True,
+        slug_field='username',
+    )
+
+    class Meta:
+        model = Review
+        fields = ('id', 'score', 'text', 'pub_date', 'author',)
+        read_only_fields = ('id', 'pub_date', 'author',)
+
+    def validate(self, data):
+        is_review_exist = Review.objects.filter(
+            author=self.context['request'].user,
+            title=self.context['view'].kwargs['title_id']
+        ).exists()
+
+        if self.context['request'].method == 'POST' and is_review_exist:
+            raise serializers.ValidationError(
+                'You cannot add review for the same title twice!')
+
+        return data
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        required=False,
+        read_only=True,
+        slug_field='username'
+    )
+
+    class Meta:
+        model = Comment
+        fields = ('id', 'text', 'pub_date', 'author',)
+        read_only_fields = ('id', 'pub_date', 'author',)
+
+
+class SignupSerializer(serializers.Serializer):
+    pass
+
+
+class JwtTokenSerializer(serializers.Serializer):
+    pass
